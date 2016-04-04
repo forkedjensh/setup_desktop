4a2f8eb89ce90a229cf1687b7c66dfbcfa3c2c19
forkedjensh <forkedjensh@mailbox.org>
2016-04-04 12:32:30 +0200

    Ensure that Xcode command line tools get installed

--- bin/bootstrap_osx.sh
+++ bin/bootstrap_osx.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-echo Installing homebrew
+echo Installing Xcode command line tools if necessary...
+gcc --version
+
+echo Installing homebrew...
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo Installing ansible...
